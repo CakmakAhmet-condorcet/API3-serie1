c21bf27a3363f4b9c64c283a72041f20ab5d1e3f
Cakmak <ahmet.cakmak@condorcet.be>
2024-10-07 08:56:46 +0200

    Exercice 1 fini

---
+++ src/main/java/Exo1/QueryReceiver.java
@@ -0,0 +1,40 @@
+package Exo1;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/QueryReceiver")
+public class QueryReceiver extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            String name = request.getParameter("name");
+            String firstname = request.getParameter("firstname");
+            String age = request.getParameter("age");
+
+            out.println("<html>");
+            out.println("<head><title>Réponse</title></head>");
+            out.println("<body>");
+            out.println("<h1>Bonjour " + firstname + " " + name + ", vous avez " + age + " ans.</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+}
